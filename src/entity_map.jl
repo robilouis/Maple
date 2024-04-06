add0a34430141a8375191535485adee8840aa057
Louis <louis.robinet@alcemy.tech>
2024-04-06 12:59:00 +0200

    added crystalheart to entitymap

--- src/entity_map.jl
+++ src/entity_map.jl
@@ -1,95 +1,44 @@
 # TODO - Add more
-default_entity_map = Dict{Char, Union{Type, Function}}(
+default_entity_map = Dict{Char,Union{Type,Function}}(
     '^' => SpikesUp,
     '<' => SpikesLeft,
     'v' => SpikesDown,
-    '>' => SpikesRight,
-
-    'P' => Player,
-
-    'D' => DreamBlock,
-    'C' => CrumbleBlock,
-
-    '_' => JumpThru,
-
-    'O' => GreenBooster,
-    'Q' => RedBooster,
-
-    'W' => Water,
-
-    'S' => Spinner,
-
-    'B' => Bumper,
-
-    'R' => Refill,
-    'F' => Feather
+    '>' => SpikesRight, 'P' => Player, 'D' => DreamBlock,
+    'C' => CrumbleBlock, '_' => JumpThru, 'O' => GreenBooster,
+    'Q' => RedBooster, 'W' => Water, 'S' => Spinner, 'B' => Bumper, 'R' => Refill,
+    'F' => Feather, 'H' => CrystalHeart
 )
 
 
 # TODO - Add more
-entity_offsets = Dict{Union{Type, Function}, Tuple{Integer, Integer}}(
+entity_offsets = Dict{Union{Type,Function},Tuple{Integer,Integer}}(
     SpikesUp => (0, 8),
     SpikesLeft => (8, 0),
     SpikesDown => (0, 0),
-    SpikesRight => (0, 0),
-
-    Player => (8, 8),
-
-    DreamBlock => (0, 0),
-    CrumbleBlock => (0, 0),
-
-    JumpThru => (0, 0),
-
-    GreenBooster => (4, 4),
-    RedBooster => (4, 4),
-
-    Refill => (4, 4),
-    Feather => (4, 4),
-
-    Water => (0, 0),
-
-    Spinner => (4, 4),
-
-    Bumper => (4, 4),
-
-    Spring => (8, 8),
+    SpikesRight => (0, 0), Player => (8, 8), DreamBlock => (0, 0),
+    CrumbleBlock => (0, 0), JumpThru => (0, 0), GreenBooster => (4, 4),
+    RedBooster => (4, 4), Refill => (4, 4),
+    Feather => (4, 4), Water => (0, 0), Spinner => (4, 4), Bumper => (4, 4), Spring => (8, 8),
     SpringLeft => (0, 8),
     SpringRight => (8, 8),
+    CrystalHeart => (0, 0)
 )
 # Function => (searchWidth, searchHeight)
-search_for_size = Dict{Union{Type, Function}, Tuple{Bool, Bool}}(
+search_for_size = Dict{Union{Type,Function},Tuple{Bool,Bool}}(
     SpikesUp => (true, false),
     SpikesDown => (true, false),
     SpikesLeft => (false, true),
-    SpikesRight => (false, true),
-
-    DreamBlock => (true, true),
+    SpikesRight => (false, true), DreamBlock => (true, true),
     CrumbleBlock => (true, false),
     DashBlock => (true, true),
-    NegaBlock => (true, true),
-
-    JumpThru => (true, false),
-
-    Barrier => (true, true),
-
-    Water => (true, true),
-
-    Spinner => (false,  false),
-
-    Refill => (false,  false),
-    Feather => (false, false),
-
-    GreenBooster => (false,  false),
-    RedBooster => (false,  false),
-
-    Spring => (false,  false),
-    SpringLeft => (false,  false),
-    SpringRight => (false,  false),
-
-    Bumper => (false, false),
+    NegaBlock => (true, true), JumpThru => (true, false), Barrier => (true, true), Water => (true, true), Spinner => (false, false), Refill => (false, false),
+    Feather => (false, false), GreenBooster => (false, false),
+    RedBooster => (false, false), Spring => (false, false),
+    SpringLeft => (false, false),
+    SpringRight => (false, false), Bumper => (false, false), CrystalHeart => (false, false)
 )
 
-function searchSize(tiles::Array{Char, 2}, x::Integer, y::Integer, searchWidth::Bool=true, searchHeight::Bool=true)
+function searchSize(tiles::Array{Char,2}, x::Integer, y::Integer, searchWidth::Bool=true, searchHeight::Bool=true)
     if !searchWidth && !searchHeight
         return 1, 1
     end
@@ -99,21 +48,21 @@ function searchSize(tiles::Array{Char, 2}, x::Integer, y::Integer, searchWidth::
 
     stopX, stopY = x, y
 
-    while searchWidth && stopX + 1 <= width && tiles[y, stopX + 1] == c
+    while searchWidth && stopX + 1 <= width && tiles[y, stopX+1] == c
         stopX += 1
     end
 
-    while searchHeight && stopY + 1 <= height && all(tiles[stopY + 1, x:stopX] .== c)
+    while searchHeight && stopY + 1 <= height && all(tiles[stopY+1, x:stopX] .== c)
         stopY += 1
     end
 
     return stopX - x + 1, stopY - y + 1
 end
 
-entityMap(s::String, lookup::Dict{Char, Union{Type, Function}}=default_entity_map) = entityMap(Tiles(s).data, lookup)
-entityMap(t::Tiles, lookup::Dict{Char, Union{Type, Function}}=default_entity_map) = entityMap(t.data, lookup)
+entityMap(s::String, lookup::Dict{Char,Union{Type,Function}}=default_entity_map) = entityMap(Tiles(s).data, lookup)
+entityMap(t::Tiles, lookup::Dict{Char,Union{Type,Function}}=default_entity_map) = entityMap(t.data, lookup)
 
-function entityMap(tiles::Array{Char, 2}, lookup::Dict{Char, Union{Type, Function}}=default_entity_map)
+function entityMap(tiles::Array{Char,2}, lookup::Dict{Char,Union{Type,Function}}=default_entity_map)
     height, width = size(tiles)
 
     complete = fill(false, (height, width))
@@ -131,7 +80,7 @@ function entityMap(tiles::Array{Char, 2}, lookup::Dict{Char, Union{Type, Functio
                 searchWidth, searchHeight = haskey(search_for_size, func) ? search_for_size[func] : (false, false)
                 tileWidth, tileHeight = searchSize(tiles, x, y, searchWidth, searchHeight)
 
-                complete[y:y + tileHeight - 1, x:x + tileWidth - 1] .= true
+                complete[y:y+tileHeight-1, x:x+tileWidth-1] .= true
 
                 if !searchWidth && !searchHeight
                     push!(res, func(x * 8 + offset[1] - 8, y * 8 + offset[2] - 8))
